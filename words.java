45aa2e820759a800f199db951fb0f44a1bd72a74
AnushaSindhu <anushaksr10@gmail.com>
2016-08-26 23:50:30 +0530

    Add files via upload

---
+++ words.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class words
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String s1[]=s.split(" ");
+for(int i=0;i<s1.length;i++)
+{
+}
+System.out.println(s1.length);
+}
+}
