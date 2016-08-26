bba858063bcc9d0ba028ea7b6573430aa38f8352
AnushaSindhu <anushaksr10@gmail.com>
2016-08-26 23:26:07 +0530

    Add files via upload

---
+++ begin11.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class begin11
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double a=sc.nextInt();
+int b=(int)Math.pow(a,2);
+System.out.print(b);
+}
+}
