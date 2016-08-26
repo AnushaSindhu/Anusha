446024438021f752811a91fe3eceec8b759296b0
AnushaSindhu <anushaksr10@gmail.com>
2016-08-26 23:53:04 +0530

    Add files via upload

---
+++ temp.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class temp
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a+" "+b);
+}
+}
