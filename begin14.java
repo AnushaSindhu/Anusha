76381cba171abb300931af039f73109e0b8e5f8e
AnushaSindhu <anushaksr10@gmail.com>
2016-08-26 23:32:51 +0530

    Add files via upload

---
+++ begin14.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class begin14
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+ArrayList<Integer> al=new ArrayList<Integer>();
+for(int i=a+1;i<b;i++)
+{
+if(i%2!=0)
+{
+al.add(i);
+}
+else
+{
+}
+}
+System.out.print(al);
+}
+}
+
