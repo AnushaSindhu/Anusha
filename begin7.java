b2728c942e31aa169cfbf4399300961aed8f1b07
AnushaSindhu <anushaksr10@gmail.com>
2016-08-26 23:20:09 +0530

    Add files via upload

---
+++ begin7.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class begin7
+{
+public static void main(String args[])
+{
+String a="Hello";
+for(int i=0;i<5;i++)
+{
+System.out.print(a);
+}
+}
+}
+
