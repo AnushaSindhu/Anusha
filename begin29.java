270b59db205c0f41b8c154f9275ac38794b4e3d4
AnushaSindhu <anushaksr10@gmail.com>
2016-09-07 11:51:29 +0530

    Add files via upload

---
+++ begin29.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class begin29
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int sum=0;
+int[] b=new int[a];
+for(int i=0;i<=a;i++)
+{
+sum=sum+b[i];
+}
+System.out.print(sum);
+}
+}
